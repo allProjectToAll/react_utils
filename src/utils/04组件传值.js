bb059bbea7c6ddeb182575b6b5061955fcb7b1e8
宋瑞丰 <1129719568@qq.com>
2021-03-31 23:52:10 +0800

    no message

---
+++ src/utils/04组件传值.js
@@ -0,0 +1,49 @@
+class ParentDemo extends React.Component{
+    constructor(props){
+      super(props)
+      this.state = {
+        list:[],
+      }
+    }
+    render(){
+      return <Fragment>
+          <ul>
+            {
+              this.state.list.map((item,index)=>(
+               <ChildDemo avname="波多野结衣" content={item} key={index} index={index} deletItem={this.deletList.bind(this)}></ChildDemo>
+              ))
+            }
+          </ul>
+      </Fragment>
+  
+    }
+    deletList(){//方法
+
+    }
+}
+// class组件传值
+class ChildDemo extends Component {
+    constructor(props){
+        super(props)
+        this.state={}
+    }
+    render() { 
+        return ( 
+            // 子组建调用父组建的方法
+            <li onClick={()=>this.deletItem()}>
+               {this.props.avname}为您做- {this.props.content}
+            </li>
+         );
+    }
+    deletItem(){
+        this.props.deletItem(this.props.index)
+    }
+}
+
+//函数组件传值
+function CmtItem(props){
+    return <div>
+        <h1>评论人：{props.user}</h1>
+        <p>评论内容：{props.content}</p>
+    </div>
+}
